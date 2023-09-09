f9c86acf2efa477e1b356d7f8294c7bd832c8ab3
Sijel <144510849+kirilavanyagorila@users.noreply.github.com>
2023-09-09 11:00:14 +0400

    версия 3
    3 фото

--- фотки/фотки/Form1.Designer.cs
+++ фотки/фотки/Form1.Designer.cs
@@ -33,8 +33,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -81,11 +84,35 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.InitialImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(266, 29);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(261, 252);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(151, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 54);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "я через 20 лет";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button1);
@@ -94,6 +121,7 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -104,6 +132,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- фотки/фотки/Form1.cs
+++ фотки/фотки/Form1.cs
@@ -21,11 +21,20 @@ namespace фотки
         {
             pictureBox1.Visible = true;
             pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             pictureBox2.Visible = true;
+            pictureBox1.Visible = false;
+            pictureBox3.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Visible = true;
+            pictureBox1.Visible = false;
             pictureBox1.Visible = false;
         }
     }
